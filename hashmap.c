3ef27f95b3cf84e8e755e3db953162e882408095
Seppe <seppe.degryse@telenet.be>
2023-05-02 19:02:17 +0200

    Improved comments.

--- hashmap.c
+++ hashmap.c
@@ -13,9 +13,6 @@ static void *(*_malloc)(size_t) = NULL;
 static void *(*_realloc)(void *, size_t) = NULL;
 static void (*_free)(void *) = NULL;
 
-// hashmap_set_allocator allows for configuring a custom allocator for
-// all hashmap library operations. This function, if needed, should be called
-// only once at startup and a prior to calling hashmap_new().
 void hashmap_set_allocator(void *(*malloc)(size_t), void (*free)(void*)) 
 {
     _malloc = malloc;
@@ -32,7 +29,6 @@ struct bucket {
     uint64_t dib:16;
 };
 
-// hashmap is an open addressed hash map using robinhood hashing.
 struct hashmap {
     void *(*malloc)(size_t);
     void *(*realloc)(void *, size_t);
@@ -69,8 +65,6 @@ static uint64_t get_hash(struct hashmap *map, const void *key) {
     return map->hash(key, map->seed0, map->seed1) << 16 >> 16;
 }
 
-// hashmap_new_with_allocator returns a new hash map using a custom allocator.
-// See hashmap_new for more information information
 struct hashmap *hashmap_new_with_allocator(
                             void *(*_malloc)(size_t), 
                             void *(*_realloc)(void*, size_t), 
@@ -134,24 +128,6 @@ struct hashmap *hashmap_new_with_allocator(
     return map;  
 }
 
-
-// hashmap_new returns a new hash map. 
-// Param `elsize` is the size of each element in the tree. Every element that
-// is inserted, deleted, or retrieved will be this size.
-// Param `cap` is the default lower capacity of the hashmap. Setting this to
-// zero will default to 16.
-// Params `seed0` and `seed1` are optional seed values that are passed to the 
-// following `hash` function. These can be any value you wish but it's often 
-// best to use randomly generated values.
-// Param `hash` is a function that generates a hash value for an item. It's
-// important that you provide a good hash function, otherwise it will perform
-// poorly or be vulnerable to Denial-of-service attacks. This implementation
-// comes with two helper functions `hashmap_sip()` and `hashmap_murmur()`.
-// Param `compare` is a function that compares items in the tree. See the 
-// qsort stdlib function for an example of how this function works.
-// The hashmap must be freed with hashmap_free(). 
-// Param `elfree` is a function that frees a specific item. This should be NULL
-// unless you're storing some kind of reference data in the hash.
 struct hashmap *hashmap_new(size_t elsize, size_t cap, 
                             uint64_t seed0, uint64_t seed1,
                             uint64_t (*hash)(const void *item, 
@@ -178,13 +154,6 @@ static void free_elements(struct hashmap *map) {
     }
 }
 
-
-// hashmap_clear quickly clears the map. 
-// Every item is called with the element-freeing function given in hashmap_new,
-// if present, to free any data referenced in the elements of the hashmap.
-// When the update_cap is provided, the map's capacity will be updated to match
-// the currently number of allocated buckets. This is an optimization to ensure
-// that this operation does not perform any allocations.
 void hashmap_clear(struct hashmap *map, bool update_cap) {
     map->count = 0;
     free_elements(map);
@@ -245,10 +214,6 @@ static bool resize(struct hashmap *map, size_t new_cap) {
     return true;
 }
 
-// hashmap_set inserts or replaces an item in the hash map. If an item is
-// replaced then it is returned otherwise NULL is returned. This operation
-// may allocate memory. If the system is unable to allocate additional
-// memory then NULL is returned and hashmap_oom() returns true.
 void *hashmap_set(struct hashmap *map, const void *item) {
     if (!item) {
         panic("item is null");
@@ -293,8 +258,6 @@ void *hashmap_set(struct hashmap *map, const void *item) {
 	}
 }
 
-// hashmap_get returns the item based on the provided key. If the item is not
-// found then NULL is returned.
 void *hashmap_get(struct hashmap *map, const void *key) {
     if (!key) {
         panic("key is null");
@@ -315,9 +278,6 @@ void *hashmap_get(struct hashmap *map, const void *key) {
 	}
 }
 
-// hashmap_probe returns the item in the bucket at position or NULL if an item
-// is not set for that bucket. The position is 'moduloed' by the number of 
-// buckets in the hashmap.
 void *hashmap_probe(struct hashmap *map, uint64_t position) {
     size_t i = position & map->mask;
     struct bucket *bucket = bucket_at(map, i);
@@ -327,9 +287,6 @@ void *hashmap_probe(struct hashmap *map, uint64_t position) {
     return bucket_item(bucket);
 }
 
-
-// hashmap_delete removes an item from the hash map and returns it. If the
-// item is not found then NULL is returned.
 void *hashmap_delete(struct hashmap *map, void *key) {
     if (!key) {
         panic("key is null");
@@ -371,14 +328,10 @@ void *hashmap_delete(struct hashmap *map, void *key) {
 	}
 }
 
-// hashmap_count returns the number of items in the hash map.
 size_t hashmap_count(struct hashmap *map) {
     return map->count;
 }
 
-// hashmap_free frees the hash map
-// Every item is called with the element-freeing function given in hashmap_new,
-// if present, to free any data referenced in the elements of the hashmap.
 void hashmap_free(struct hashmap *map) {
     if (!map) return;
     free_elements(map);
@@ -386,15 +339,10 @@ void hashmap_free(struct hashmap *map) {
     map->free(map);
 }
 
-// hashmap_oom returns true if the last hashmap_set() call failed due to the 
-// system being out of memory.
 bool hashmap_oom(struct hashmap *map) {
     return map->oom;
 }
 
-// hashmap_scan iterates over all items in the hash map
-// Param `iter` can return false to stop iteration early.
-// Returns false if the iteration has been stopped early.
 bool hashmap_scan(struct hashmap *map, 
                   bool (*iter)(const void *item, void *udata), void *udata)
 {
@@ -409,25 +357,6 @@ bool hashmap_scan(struct hashmap *map,
     return true;
 }
 
-
-// hashmap_iter iterates one key at a time yielding a reference to an
-// entry at each iteration. Useful to write simple loops and avoid writing
-// dedicated callbacks and udata structures, as in hashmap_scan.
-//
-// map is a hash map handle. i is a pointer to a size_t cursor that
-// should be initialized to 0 at the beginning of the loop. item is a void
-// pointer pointer that is populated with the retrieved item. Note that this
-// is NOT a copy of the item stored in the hash map and can be directly
-// modified.
-//
-// Note that if hashmap_delete() is called on the hashmap being iterated,
-// the buckets are rearranged and the iterator must be reset to 0, otherwise
-// unexpected results may be returned after deletion.
-//
-// This function has not been tested for thread safety.
-//
-// The function returns true if an item was retrieved; false if the end of the
-// iteration has been reached.
 bool hashmap_iter(struct hashmap *map, size_t *i, void **item)
 {
     struct bucket *bucket;
@@ -597,15 +526,13 @@ static uint64_t MM86128(const void *key, const int len, uint32_t seed) {
     return (((uint64_t)h2)<<32)|h1;
 }
 
-// hashmap_sip returns a hash value for `data` using SipHash-2-4.
-uint64_t hashmap_sip(const void *data, size_t len, 
+uint64_t hashmap_sip(const void *data, size_t len,
                      uint64_t seed0, uint64_t seed1)
 {
     return SIP64((uint8_t*)data, len, seed0, seed1);
 }
 
-// hashmap_murmur returns a hash value for `data` using Murmur3_86_128.
-uint64_t hashmap_murmur(const void *data, size_t len, 
+uint64_t hashmap_murmur(const void *data, size_t len,
                         uint64_t seed0, uint64_t seed1)
 {
     (void)seed1;
--- hashmap.h
+++ hashmap.h
@@ -9,8 +9,20 @@
 #include <stddef.h>
 #include <stdint.h>
 
+/// \author Joshua J Baker
+/// An open addressed hash map using robinhood hashing.
 struct hashmap;
 
+/// Creates a new hashmap.
+/// \param elsize The size of each element in the tree.
+/// \param cap The default lower capacity of the hashmap. Setting this to zero will default to 16.
+/// \param seed0 Optional seed value passed on to the hash function.
+/// \param seed1 Optional seed value passed on to the hash function.
+/// \param hash The hash function used for the hashmap.
+/// \param compare The function that compares items in the tree. See the qsort stdlib function for an example.
+/// \param elfree The function that frees a specific item. This should be NULL unless referenced data is stored in the hash.
+/// \param udata A pointer to user-defined data that can be passed to the element comparison and element free functions.
+/// \return A pointer to a new hashmap.
 struct hashmap *hashmap_new(size_t elsize, size_t cap, 
                             uint64_t seed0, uint64_t seed1,
                             uint64_t (*hash)(const void *item, 
@@ -19,6 +31,20 @@ struct hashmap *hashmap_new(size_t elsize, size_t cap,
                                            void *udata),
                             void (*elfree)(void *item),
                             void *udata);
+
+/// Creates a hashmap with an custom allocator.
+/// \param malloc A pointer to the allocation function.
+/// \param realloc A pointer to the reallocation function.
+/// \param free A pointer to the free function.
+/// \param elsize The size of each element in the tree.
+/// \param cap The default lower capacity of the hashmap. Setting this to zero will default to 16.
+/// \param seed0 Optional seed value passed on to the hash function.
+/// \param seed1 Optional seed value passed on to the hash function.
+/// \param hash The hash function used for the hashmap.
+/// \param compare The function that compares items in the tree. See the qsort stdlib function for an example.
+/// \param elfree The function that frees a specific item. This should be NULL unless referenced data is stored in the hash.
+/// \param udata A pointer to user-defined data that can be passed to the element comparison and element free functions.
+/// \return A pointer to a new hashmap.
 struct hashmap *hashmap_new_with_allocator(
                             void *(*malloc)(size_t), 
                             void *(*realloc)(void *, size_t), 
@@ -31,25 +57,103 @@ struct hashmap *hashmap_new_with_allocator(
                                            void *udata),
                             void (*elfree)(void *item),
                             void *udata);
+
+/// Frees the hash map.
+/// \param map The hash map to be freed.
 void hashmap_free(struct hashmap *map);
+
+/// Quckly clears the whole map.
+/// \details Every item is called with the element-freeing function given in hashmap_new,
+/// if present, to free any data referenced in the elements of the hashmap.
+/// \param map A pointer to the map to be cleared.
+/// \param update_cap The new capacity of the hash map (optional).
+/// \post  When the update_cap is provided, the map's capacity will be updated to match
+/// the currently number of allocated buckets. This is an optimization to ensure
+/// that this operation does not perform any allocations.
 void hashmap_clear(struct hashmap *map, bool update_cap);
+
+/// Getter for the number of items in the hash map.
+/// \param map A pointer to the to get the count from.
+/// \return The number of elements in the map.
 size_t hashmap_count(struct hashmap *map);
+
+/// Checker if the system is out of memory.
+/// \param map A pointer to the map to be checked.
+/// \return True if the system is out of memory, false if not.
 bool hashmap_oom(struct hashmap *map);
-void *hashmap_get(struct hashmap *map, const void *item);
+
+/// Gets an item out of the map with the given key.
+/// \param map A pointer to he map to get an element out of.
+/// \param key The key of the item to be found.
+/// \return The item if found, NULL if the item is not found.
+/// \pre Key may not be NULL.
+void *hashmap_get(struct hashmap *map, const void *key);
+
+/// Inserts or replaces an item in the hash map.
+/// \param map A pointer to the map to insert or replace an item in.
+/// \param item The item to be added to the list.
+/// \return The item that is replaced, NULL if no item is replaced.
+/// \pre Key may not be NULL.
+/// \post If the map has reached 75% of its capacity,
+/// it resizes the map to double its current size to make room for new entries.
 void *hashmap_set(struct hashmap *map, const void *item);
-void *hashmap_delete(struct hashmap *map, void *item);
+
+/// Deletes an item from the hash map.
+/// \param map A pointer to the map to delete an item from.
+/// \param key The key of the item to be deleted.
+/// \return The deleted item, NULL if the item is not found.
+void *hashmap_delete(struct hashmap *map, void *key);
+
+/// Gets the item in the bucket at a certain position.
+/// \param map A pointer to the hashmap.
+/// \param position The position of the bucket.
+/// The position is reduced to position modulo number of buckets.
+/// \return The item, or NULL if no item is present at that bucket.
 void *hashmap_probe(struct hashmap *map, uint64_t position);
+
+/// Scanner for the hash map.
+/// \param map A pointer to the map to be scanned.
+/// \param iter A pointer to the user-provided callback function to be called for each item.
+/// \param udata A pointer to user-provided data, which will be passed to the callback function.
+/// \return True if the iteration was completed normally, false if it was stopped early.
 bool hashmap_scan(struct hashmap *map,
                   bool (*iter)(const void *item, void *udata), void *udata);
+
+/// Iterator for the hash map.
+/// \param map A pointer to the hash map to be iterated.
+/// \param i A pointer to a size_t cursor that should be initialized to 0 at the beginning of the loop.
+/// \param item A void pointer pointer that is populated with the retrieved item. Note that this
+/// is NOT a copy of the item stored in the hash map and can be directly modified.
+/// \return True if an item was retrieved, false if the end of the iteration has been reached.
+/// \note Note that if hashmap_delete() is called on the hashmap being iterated,
+/// the buckets are rearranged and the iterator must be reset to 0, otherwise
+/// unexpected results may be returned after deletion.
+/// \warning This function has not been tested for thread safety.
 bool hashmap_iter(struct hashmap *map, size_t *i, void **item);
 
+/// Creates a hash value using SipHash-2-4.
+/// \param data The data used for hash generation.
+/// \param len The length of the data.
+/// \param seed0 A seed for the hashing algorithm (optional).
+/// \param seed1 A seed for the hashing algorithm (optional).
+/// \return The hash of the data.
 uint64_t hashmap_sip(const void *data, size_t len, 
                      uint64_t seed0, uint64_t seed1);
+
+/// Creates a hash value using Murmur3_86_128.
+/// \param data The data used for hash generation.
+/// \param len The length of the data.
+/// \param seed0 A seed for the hashing algorithm (optional).
+/// \param seed1 A seed for the hashing algorithm (optional).
+/// \return The hash of the data.
 uint64_t hashmap_murmur(const void *data, size_t len, 
                         uint64_t seed0, uint64_t seed1);
 
 
-// DEPRECATED: use `hashmap_new_with_allocator`
+/// This function allows for configuring a custom allocator for all hashmap library operations.
+/// \param malloc A pointer to the allocation function.
+/// \param free A pointer to the free function.
+/// \deprecated Use `hashmap_new_with_allocator`
 void hashmap_set_allocator(void *(*malloc)(size_t), void (*free)(void*));
 
 #endif
